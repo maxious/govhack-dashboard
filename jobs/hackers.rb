8a67e2d9eba7c9e7ee8c0dbdc0eeeb962f77e8b9
Maxious <maxious@lambdacomplex.org>
2013-05-31 21:39:58 +1000

    Add hackers

---
+++ jobs/hackers.rb
@@ -0,0 +1,8 @@
+current_hackers = 0
+
+SCHEDULER.every '30s' do
+  last_hackers     = current_hackers
+  current_hackers     = `curl --silent http://hack.govhack.org/?q=hackers | grep -c views-row`
+
+  send_event('hackers', { current: current_hackers, last: last_hackers })
+end
--- jobs/teams.rb
+++ jobs/teams.rb
@@ -2,7 +2,7 @@ current_teams = 0
 
 SCHEDULER.every '30s' do
   last_teams     = current_teams
-  current_teams     = `curl --silent http://hack.govhack.org/?q=groups | grep -c field-name-field-team-name`
+  current_teams     = `curl --silent http://hack.govhack.org/?q=teams | grep -c views-row`
 
   send_event('teams', { current: current_teams, last: last_teams })
 end
